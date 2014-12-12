c61b35132b3fdef7b3a41544574d3a3f74c32717
HydraOrc <hydra0@bigmir.net>
2014-12-12 21:09:24 +0200

    main page events

--- public/js/app/user/modules/main/main.js
+++ public/js/app/user/modules/main/main.js
@@ -6,8 +6,45 @@ export default Controller.extend(
 			
 		}
 	}, {
+		variables: function() {
+			this.pages = this.element.find('.page');
+			this.page = 1;
+			
+			this.active = 'active';
+		},
+		
 		after_init: function(data) {
 			
+		},
+		
+		'.arrow.left click': 'prev_page',
+		
+		'.arrow.right click': 'next_page',
+		
+		prev_page: function() {
+			if(this.page == 1) {
+				this.page = this.pages.length;
+			} else {
+				this.page--;
+			}
+			
+			this.set_page();
+		},
+		
+		next_page: function() {
+			if(this.page == this.pages.length) {
+				this.page = 1;
+			} else {
+				this.page++;
+			}
+			
+			this.set_page();
+		},
+		
+		set_page: function() {
+			console.log(this.page)
+			this.pages.removeClass(this.active);
+			this.pages.filter('.page_' + this.page).addClass(this.active);
 		}
     }
 );
